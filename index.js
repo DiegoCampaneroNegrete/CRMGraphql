79c77bac5a91149236d6c383508a0479df3612e0
Diego Alejandro Campanero Negrete <diegoa.campaneron@gmail.com>
2021-07-29 18:14:07 -0500

    Add context and implement token

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const {ApolloServer, gql} = require('apollo-server');
+const jwt = require('jsonwebtoken');
 const typeDefs = require('./DB/schemas')
 const resolvers = require('./DB/resolvers');
 const connectDB = require('./config/db');
-
+require('dotenv').config({path: 'variables.env'})
 // Conectar a la BD
 connectDB();
 
@@ -10,17 +11,24 @@ connectDB();
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => {
-        const myContext = 'Hola';
-        return {
-            myContext
-        };
+    context: ({req}) => {
+        const token = req.headers['authorization'] || '';
+        if(token) {
+            try {
+                const user = jwt.verify(token, process.env.SECRET_WORD);
+                return {
+                    user
+                };
+            } catch (error) {
+                console.log('Context Toke Error', error);
+            }
+        }
     }
 });
 
 // Arrancando el servidor
 server.listen().then((url) => {
     // console.log(`Servirod listo en la URL ${url}`);
-    console.log(`Servirod listo en la URL`, url);
+    // console.log(`Servirod listo en la URL`, url);
 });
 
